01a7623a162f36fb8f3486060f04f72f19101455
Yogesh Khater <yogesh@joshsoftware.com>
2019-08-15 12:59:51 +0530

    Updates in renderer API
    Pass current step's output including next and previous step accordingly

--- lib/trailblazer/activity/present.rb
+++ lib/trailblazer/activity/present.rb
@@ -11,29 +11,33 @@ module Trailblazer
         INDENTATION = "   |".freeze
         STEP_PREFIX = "-- ".freeze
 
-        def default_renderer(stack:, level:, input:, name:, **)
-          [ level, %{#{name}} ]
+        def default_renderer(step:, previous_step:, next_step:)
+          [ step[:level], %{#{step[:name]}} ]
         end
 
         def call(stack, level: 1, tree: [], renderer: method(:default_renderer), **options)
           tree(stack.to_a, level, tree: tree, renderer: renderer, **options)
         end
 
-        def tree(stack, level, tree:, **options)
+        def tree(stack, level, tree:, renderer:, **options)
           tree_for(stack, level, options.merge(tree: tree))
 
-          render_tree(tree: tree, level: level)
+          steps = tree.each_with_index.map do |step, index|
+            renderer.(step: step, previous_step: tree[index - 1], next_step: tree[index + 1])
+          end
+
+          render_tree_for(steps)
         end
 
-        def render_tree(tree:, level:)
-          tree.map { |level, step|
+        def render_tree_for(steps)
+          steps.map { |level, step|
             indentation = INDENTATION * (level -1)
             indentation = indentation[0...-1] + "`" if level == 1 || /End./.match(step) # start or end step
             indentation + STEP_PREFIX + step
           }.join("\n")
         end
 
-        def tree_for(stack, level, tree:, renderer:, **options)
+        def tree_for(stack, level, tree:, **options)
           stack.each do |lvl| # always a Stack::Task[input, ..., output]
             input, output, nested = Trace::Level.input_output_nested_for_level(lvl)
 
@@ -44,33 +48,14 @@ module Trailblazer
             name = (node = graph.find { |node| node[:task] == task }) ? node[:id] : task
             name ||= task # FIXME: bullshit
 
-            tree << renderer.(stack: stack, level: level, input: input, name: name, **options)
+            tree << { level: level, input: input, output: output, name: name, **options }
 
             if nested.any? # nesting
-              tree_for(nested, level + 1, options.merge(tree: tree, renderer: renderer))
+              tree_for(nested, level + 1, options.merge(tree: tree))
             end
 
             tree
           end
-        end
-
-        def color_map
-          {
-            Trailblazer::Activity::Start => :blue,
-            Trailblazer::Activity::End   => :pink,
-            Trailblazer::Activity::Right => :green,
-            Trailblazer::Activity::Left  => :red
-          }
-        end
-
-        def color_table
-          {
-            red:    31,
-            green:  32,
-            yellow: 33,
-            blue:   34,
-            pink:   35
-          }
         end
       end
     end
